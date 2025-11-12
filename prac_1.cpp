c2a4d70d8421e8de5fc578a4a679be1a8458daab
Prashant Yadav <119166313+Prashant-yadv@users.noreply.github.com>
2025-11-12 22:49:47 +0530

    Add files via upload

---
+++ prac_1.cpp
@@ -0,0 +1,13 @@
+//  First 10 Natural Numbers
+
+#include<iostream>
+using namespace std;
+int main(){
+int x;
+cout<<"Enter the x ";
+cin>>x;
+cout<<"First 10 Natural Numbers Are :";
+for(int i=0;i<10;i++){
+    cout<<i<<"\n";
+}
+}
